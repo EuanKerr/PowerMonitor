cf530061e957e21b2f9905d9c5f25c8b1d4db7c8
euankerr <ekerr@gdssecurity.com>
2017-12-13 15:06:59 +0000

    add

--- monitor.py
+++ monitor.py
@@ -5,8 +5,16 @@ import time
 import radio
 import Devices
 import OpenThings
+import mysql.connector
+from datetime import date, datetime
+from time import strftime
 
-APP_DELAY    = 2
+db_host = "localhost"
+db_user = "powerUser"
+db_passwd = ""
+db_database = "power"
+
+APP_DELAY    = 30
 
 def loop(receive_time=1):
     radio.receiver(fsk=True)
@@ -43,15 +51,37 @@ def energy_monitor_loop():
     msg = loop()
 
     if not msg:
+	#print("pass")
         pass
     else:
         power      = msg[0]["value"]
+	if power > 1000:
+	    print("over 1000W")
+	    return False
+	if power < 20:
+	    print("less than 0W")
+	    return False
         voltage    = msg[2]["value"]
         frequency  = round(msg[3]["value"], 0)
         # print("Power: {power}, Voltage: {voltage}, Frequency: {frequency}".format(power=power, voltage=voltage, frequency=frequency))
-        print("{power}, {voltage}, {frequency}".format(power=power, voltage=voltage, frequency=frequency))
+        # print("{power}, {voltage}, {frequency}".format(power=power, voltage=voltage, frequency=frequency))
+        try:
+            sqlConn = mysql.connector.connect(host=db_host, user=db_user, passwd=db_passwd, db=db_database)
+            sqlCursor = sqlConn.cursor()
+        except Exception, e:
+            print "{}: Unexpected error 2 (connecting to DB): {}".format(datetime.now(), e)
+            return
 
-    time.sleep(APP_DELAY)
+        try:
+            sqlCursor.execute("INSERT INTO server_power (powerTime, powerDate, powerValue, voltageValue, frequencyValue) VALUES(%s,%s,%s,%s,%s)", (strftime("%Y-%m-%d %H:%M:%S"), date.today(), power, voltage, frequency))
+            sqlConn.commit()
+        except Exception, e:
+            print "{}: Unexpected error 1 (Inserting into DB): {}".format(datetime.now(), e)
+        sqlConn.close()
+        return True
+
+    # time.sleep(APP_DELAY)
+    return False
 
 
 if __name__ == "__main__":
@@ -60,7 +90,7 @@ if __name__ == "__main__":
     OpenThings.init(Devices.CRYPT_PID)
 
     try:
-        while True:
+        while energy_monitor_loop() is False:
             energy_monitor_loop()
     finally:
         radio.finished()
